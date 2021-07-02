c7199f57e35366121bbda8d425efb8113237eecc
lindsey98 <liny@xgpd5.comp.nus.edu.sg>
2021-07-02 16:03:16 +0800

    Customized dataset

---
+++ src/powerful_benchmarker/datasets/logo.py
@@ -0,0 +1,27 @@
+from torch.utils.data import Dataset
+from torchvision import datasets
+import numpy as np
+
+class LogoDataset(Dataset):
+    def __init__(self, root, transform=None, download=False):
+        
+        original_test_dataset = datasets.ImageFolder(os.path.join(root, 'test'), transform=transform)
+        original_train_dataset = datasets.ImageFolder(os.path.join(root, 'train'), transform=transform) 
+        self.dataset = torch.utils.data.ConcatDataset([original_test_dataset, original_train_dataset])
+        self.test_set_indices = np.arange(len(original_test_dataset))
+
+        # these look useless, but are required by powerful-benchmarker
+        self.labels = np.array([b for (a, b) in self.dataset.imgs])
+        self.transform = transform 
+     
+
+    def get_split_indices(self, split_name):
+        if split_name == "test":
+            return self.test_set_indices
+        return None
+    
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        return self.dataset[idx]
